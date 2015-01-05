972b8e5c15772e5a81128c0cec51192bcc267492
Kostya Stankevych <kostya@zedarus.ru>
2015-01-05 14:58:10 +0200

    Fix bug in UI animation

--- User Interface/Animations/UIRendererAnimation.cs
+++ User Interface/Animations/UIRendererAnimation.cs
@@ -6,35 +6,35 @@ namespace Zedarus.ToolKit.UserInterface
 	[AddComponentMenu("ZTK/UI/Renderer Animation")]
 	public class UIRendererAnimation : UIElementAnimation
 	{
-		private UISprite[] _sprites;
+		private UIWidget[] _widgets;
 
 		public override void Init()
 		{
-			_sprites = GetComponentsInChildren<UISprite>();
+			_widgets = GetComponentsInChildren<UIWidget>();
 			base.Init();
 		}
 
 		#region Controls
 		public override void Show()
 		{
-			foreach (UISprite sprite in _sprites)
-				sprite.enabled = true;
+			foreach (UIWidget widget in _widgets)
+				widget.enabled = true;
 			base.Show();
 		}
 		
 		public override void Hide()
 		{
-			foreach (UISprite sprite in _sprites)
-				sprite.enabled = false;
+			foreach (UIWidget widget in _widgets)
+				widget.enabled = false;
 			base.Hide();
 		}
 		
 		public override void Reset()
 		{
-			if (_sprites != null)
+			if (_widgets != null)
 			{
-				foreach (UISprite sprite in _sprites)
-					sprite.enabled = false;
+				foreach (UIWidget widget in _widgets)
+					widget.enabled = false;
 			}
 			base.Reset();
 		}
