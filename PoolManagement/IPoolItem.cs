9b82d032159341f3c6505c167c8a1625c5f5db36
Kostya Stankevych <kostya@zedarus.ru>
2015-02-04 15:10:34 +0200

    Implement simple poolmanager

---
+++ PoolManagement/IPoolItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Zedarus.ToolKit.PoolManagement
+{
+	public interface IPoolItem
+	{
+		void Init();
+		void Activate(Vector3 position);
+		void Deactivate();
+	}
+}
