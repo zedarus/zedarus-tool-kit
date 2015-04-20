ead9c1b3c4456ffcc3f178699de0ab7207f2b6cf
zedarus <kostya@zedarus.ru>
2015-04-20 14:27:57 +0300

    Create app controller class

---
+++ AppController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zedarus.ToolKit.API;
+using Zedarus.ToolKit.Data;
+
+namespace Zedarus.ToolKit
+{
+	public class AppController : MonoBehaviour
+	{
+		#region Properties
+		static private bool initialized = false;
+		#endregion
+
+		#region Unity Methods
+		private void Start()
+		{
+			if (!initialized)
+				Init();
+
+			Launch();
+			Destroy(gameObject);
+		}
+		#endregion
+
+		#region Init
+		private void Init()
+		{
+			APIManager.Instance.Init();
+
+			InitGameData();
+			InitPlayerData("default.dat");
+			
+			initialized = true;
+		}
+
+		protected virtual void InitGameData()
+		{
+
+		}
+
+		protected virtual void InitPlayerData(string filename, APIState customAPIState = null)
+		{
+			DataManager.Instance.Load(filename);
+			if (customAPIState == null)
+			{
+				DataManager.Instance.Player.AddModel<APIState>();
+				customAPIState = DataManager.Instance.Player.GetModel<APIState>();
+			}
+			APIManager.Instance.UseState(customAPIState);
+		}
+
+		protected virtual void Launch()
+		{
+			
+		}
+		#endregion
+	}
+}
