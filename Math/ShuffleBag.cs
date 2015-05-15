eb602897ee4d020a97c77d3ff672acea71caacf5
zedarus <kostya@zedarus.ru>
2015-05-15 11:10:52 +0300

    Add ShuffleBag class helper

---
+++ Math/ShuffleBag.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Zedarus.ToolKit.Math
+{
+	public class ShuffleBag<T>
+	{
+		private System.Random random = new System.Random();
+		private List<T> data;
+
+		private int currentPosition = -1;
+
+		public int Size { get { return data.Count; } }
+
+		public ShuffleBag()
+		{
+			data = new List<T>();
+		}
+
+		public void Clear()
+		{
+			data.Clear();
+		}
+
+		public void Add(T item, int amount)
+		{
+			for (int i = 0; i < amount; i++)
+				data.Add(item);
+
+			currentPosition = Size - 1;
+		}
+
+		public T Next()
+		{
+			T currentItem; 
+
+			if (currentPosition < 1)
+			{
+				currentPosition = Size - 1;
+				currentItem = data[0];
+
+				return currentItem;
+			}
+
+			var pos = random.Next(currentPosition);
+
+			currentItem = data[pos];
+			data[pos] = data[currentPosition];
+			data[currentPosition] = currentItem;
+			currentPosition--;
+
+			return currentItem;
+		}
+	}
+}
