8359f69828339384f592cb4063314a438f3d0b83
Kostya Stankevych <kostya@zedarus.ru>
2016-05-25 18:34:56 +0300

    Add parameters for scene manager

--- ScenesManagement/ScenesManager.cs
+++ ScenesManagement/ScenesManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 using Zedarus.ToolKit;
 
 namespace Zedarus.ToolKit.ScenesManagement
 {
 	public class ScenesManager : SimpleSingleton<ScenesManager>
 	{
-		#region Parameters
+		#region Properties
 		private string _nextScene = null;
 		private string _currentScene = null;
 		private string _defaultScene = null;
@@ -15,12 +16,38 @@ namespace Zedarus.ToolKit.ScenesManagement
 		private string _previousScene = null;
 		private bool _useLoadingScene = false;
 		#endregion
+
+		private Dictionary<string, Dictionary<object, object>> _params = new Dictionary<string, Dictionary<object, object>>();
 		
 		#region General Scene Switch Methods
-		public virtual void ShowScene(string sceneName) 
+		public virtual void ShowScene(string sceneName, params object[] data) 
 		{
 			_previousScene = _currentScene;
 			_currentScene = _nextScene = sceneName;
+
+			if (_params.ContainsKey(sceneName))
+			{
+				_params[sceneName].Clear();
+			}
+			else
+			{
+				_params[sceneName] = new Dictionary<object, object>();
+			}
+
+			if (data != null && data.Length > 0)
+			{
+				if (data.Length % 2 == 0)
+				{
+					for (int i = 0; i < data.Length; i += 2)
+					{
+						_params[sceneName].Add(data[i], data[i + 1]);	
+					}
+				}
+				else
+				{
+					throw new UnityException("Each scene parameter must have a name pair");
+				}
+			}
 
 			if (_useLoadingScene)
 				SceneManager.LoadScene(LoadingSceneName);
@@ -39,6 +66,27 @@ namespace Zedarus.ToolKit.ScenesManagement
 			{
 				ZedLogger.Log("No previous scene is set, switching to default screen instead.");
 				ShowDefaultScene();
+			}
+		}
+		#endregion
+
+		#region Parameters
+		public T GetSceneParam<T>(string sceneName, object paramName, T defaultValue = default(T))
+		{
+			if (_params.ContainsKey(sceneName))
+			{
+				if (_params[sceneName].ContainsKey(paramName))
+				{
+					return (T)_params[sceneName][paramName];
+				}
+				else
+				{
+					return defaultValue;
+				}
+			}
+			else
+			{
+				return defaultValue;
 			}
 		}
 		#endregion
