cf3e9b21f64887c2a17540e1a7d05a040b03811b
Kostya Stankevych <kostya@zedarus.ru>
2016-07-06 22:39:16 +0300

    Update localisation

--- Localisation/LocalisationManager.cs
+++ Localisation/LocalisationManager.cs
@@ -21,14 +21,18 @@ namespace Zedarus.ToolKit.Localisation
 		{
 			if (!_ready)
 			{
+				#if API_LOC_M2H
 				EventManager.SendEvent<string>(IDs.Events.SetLanguage, Language.CurrentLanguage().ToString());
+				#endif
 				_ready = true;
 			}
 		}
 
-		public void ChangeLanguage(LanguageCode language, bool notifyAllObjects)
+		public void ChangeLanguage(LangCode language, bool notifyAllObjects)
 		{
-			Language.SwitchLanguage(language);
+			#if API_LOC_M2H
+			Language.SwitchLanguage(language.ToString());
+			#endif
 			EventManager.SendEvent<string>(IDs.Events.SetLanguage, language.ToString());
 
 			if (notifyAllObjects)
@@ -48,7 +52,11 @@ namespace Zedarus.ToolKit.Localisation
 
 		public string Localise(string key, string page)
 		{
+			#if API_LOC_M2H
 			return Language.Get(key, page);
+			#else
+			return "no localisation api";
+			#endif
 		}
 
 		public string Localise(string key, object enumerablePage)
@@ -91,10 +99,14 @@ namespace Zedarus.ToolKit.Localisation
 			{
 				if (_firstPage == null)
 				{
+					#if API_LOC_M2H
 					List<string> list = new List<string>(Language.Sheets.Keys);
 					_firstPage = list[list.Count - 1];
 					list.Clear();
 					list = null;
+					#else
+					_firstPage = "";
+					#endif
 				}
 
 				return _firstPage;
@@ -119,6 +131,7 @@ namespace Zedarus.ToolKit.Localisation
 		{
 			_data.Clear();
 
+			#if API_LOC_M2H
 			Language.LoadAvailableLanguages();
 			Language.SwitchLanguage(LanguageCode.EN);
 
@@ -134,6 +147,7 @@ namespace Zedarus.ToolKit.Localisation
 
 				_data.Add(sheet.Key, phrases);
 			}
+			#endif
 		}
 
 		public string[] Sheets
@@ -154,5 +168,216 @@ namespace Zedarus.ToolKit.Localisation
 				return null;
 		}
 		#endif
+
+		#region Settings
+		public enum LangCode
+		{
+			N,//null
+			AA, //Afar
+			AB, //Abkhazian
+			AF, //(Zuid) Afrikaans
+			AM, //Amharic
+			AR, //Arabic
+			AR_SA, //* Arabic (Saudi Arabia)
+			AR_EG, //* Arabic (Egypt)
+			AR_DZ, //* Arabic (Algeria)
+			AR_YE, //* Arabic (Yemen)
+			AR_JO, //* Arabic (Jordan)
+			AR_KW, //* Arabic (Kuwait)
+			AR_BH, //* Arabic (Bahrain)
+			AR_IQ, //* Arabic (Iraq)
+			AR_MA, //* Arabic (Libya) 
+			AR_LY, //* Arabic (Morocco)
+			AR_OM, //* Arabic (Oman)
+			AR_SY, //* Arabic (Syria)
+			AR_LB, //* Arabic (Lebanon)
+			AR_AE, //* Arabic (U.A.E.)
+			AR_QA, //* Arabic (Qatar)
+			AS, //Assamese
+			AY, //Aymara
+			AZ, //Azerbaijani
+			BA, //Bashkir
+			BE, //Byelorussian
+			BG, //Bulgarian
+			BH, //Bihari
+			BI, //Bislama
+			BN, //Bengali
+			BO, //Tibetan
+			BR, //Breton
+			CA, //Catalan
+			CO, //Corsican
+			CS, //Czech
+			CY, //Welch
+			DA, //Danish
+			DE, //German
+			DE_AT, //* German (Austria)
+			DE_LI, //* German (Liechtenstein)
+			DE_CH, //* German (Switzerland)
+			DE_LU, //* German (Luxembourg)
+			DZ, //Bhutani
+			EL, //Greek
+			EN, //English
+			EN_US, //* English (United States)
+			EN_AU, //* English (Australia)
+			EN_NZ, //* English (New Zealand)
+			EN_ZA, //* English (South Africa)
+			EN_CB, //* English (Caribbean)
+			EN_TT, //* English (Trinidad)
+			EN_GB, //* English (United Kingdom)
+			EN_CA, //* English (Canada)
+			EN_IE, //* English (Ireland)
+			EN_JM, //* English (Jamaica)
+			EN_BZ, //* English (Belize)
+			EO, //Esperanto
+			ES, //Spanish (Spain)
+			ES_MX, //* Spanish (Mexico)
+			ES_CR, //* Spanish (Costa Rica)
+			ES_DO, //* Spanish (Dominican Republic)
+			ES_CO, //* Spanish (Colombia)
+			ES_AR, //* Spanish (Argentina)	
+			ES_CL, //* Spanish (Chile)	
+			ES_PY, //* Spanish (Paraguay)	
+			ES_SV, //* Spanish (El Salvador)	
+			ES_NI, //* Spanish (Nicaragua)	
+			ES_GT, //* Spanish (Guatemala)	
+			ES_PA, //* Spanish (Panama)	
+			ES_VE, //* Spanish (Venezuela)	
+			ES_PE, //* Spanish (Peru)
+			ES_EC, //* Spanish (Ecuador)
+			ES_UY, //* Spanish (Uruguay)
+			ES_BO, //* Spanish (Bolivia)
+			ES_HN, //* Spanish (Honduras)
+			ES_PR, //* Spanish (Puerto Rico)
+			ET, //Estonian
+			EU, //Basque
+			FA, //Persian
+			FI, //Finnish
+			FJ, //Fiji
+			FO, //Faeroese
+			FR, //French (Standard)
+			FR_BE, //* French (Belgium)
+			FR_CH, //* French (Switzerland)
+			FR_CA, //* French (Canada)
+			FR_LU, //* French (Luxembourg)
+			FY, //Frisian
+			GA, //Irish
+			GD, //Scots Gaelic
+			GL, //Galician
+			GN, //Guarani
+			GU, //Gujarati
+			HA, //Hausa
+			HI, //Hindi
+			HE, //Hebrew
+			HR, //Croatian
+			HU, //Hungarian
+			HY, //Armenian
+			IA, //Interlingua
+			ID, //Indonesian
+			IE, //Interlingue
+			IK, //Inupiak
+			IN, //former Indonesian
+			IS, //Icelandic
+			IT, //Italian
+			IT_CH, //* Italian (Switzerland)
+			IU, //Inuktitut (Eskimo)
+			IW, //DEPRECATED: former Hebrew
+			JA, //Japanese
+			JI, //DEPRECATED: former Yiddish
+			JW, //Javanese
+			KA, //Georgian
+			KK, //Kazakh
+			KL, //Greenlandic
+			KM, //Cambodian
+			KN, //Kannada
+			KO, //Korean
+			KS, //Kashmiri
+			KU, //Kurdish
+			KY, //Kirghiz
+			LA, //Latin
+			LN, //Lingala
+			LO, //Laothian
+			LT, //Lithuanian
+			LV, //Latvian, Lettish
+			MG, //Malagasy
+			MI, //Maori
+			MK, //Macedonian
+			ML, //Malayalam
+			MN, //Mongolian
+			MO, //Moldavian
+			MR, //Marathi
+			MS, //Malay
+			MT, //Maltese
+			MY, //Burmese
+			NA, //Nauru
+			NE, //Nepali
+			NL, //Dutch (Standard)
+			NL_BE, //*  Dutch (Belgium)
+			NO, //Norwegian
+			OC, //Occitan
+			OM, //(Afan) Oromo
+			OR, //Oriya
+			PA, //Punjabi
+			PL, //Polish
+			PS, //Pashto, Pushto
+			PT, //Portuguese
+			PT_BR, //* BRazilian PT - Only used if you manually set it
+			QU, //Quechua
+			RM, //Rhaeto-Romance
+			RN, //Kirundi
+			RO, //Romanian
+			RO_MO, //* Romanian (Republic of Moldova)
+			RU, //Russian
+			RU_MO, //* Russian (Republic of Moldova)
+			RW, //Kinyarwanda
+			SA, //Sanskrit
+			SD, //Sindhi
+			SG, //Sangro
+			SH, //Serbo-Croatian
+			SI, //Singhalese
+			SK, //Slovak
+			SL, //Slovenian
+			SM, //Samoan
+			SN, //Shona
+			SO, //Somali
+			SQ, //Albanian
+			SR, //Serbian
+			SS, //Siswati
+			ST, //Sesotho
+			SU, //Sudanese
+			SV, //Swedish
+			SV_FI, //* Swedish (finland)
+			SW, //Swahili
+			TA, //Tamil
+			TE, //Tegulu
+			TG, //Tajik
+			TH, //Thai
+			TI, //Tigrinya
+			TK, //Turkmen
+			TL, //Tagalog
+			TN, //Setswana
+			TO, //Tonga
+			TR, //Turkish
+			TS, //Tsonga
+			TT, //Tatar
+			TW, //Twi
+			UG, //Uigur
+			UK, //Ukrainian
+			UR, //Urdu
+			UZ, //Uzbek
+			VI, //Vietnamese
+			VO, //Volapuk
+			WO, //Wolof
+			XH, //Xhosa
+			YI, //Yiddish
+			YO, //Yoruba
+			ZA, //Zhuang
+			ZH, //Chinese Simplified
+			ZH_TW, //* Chinese Traditional / Chinese (Taiwan)
+			ZH_HK, //* Chinese (Hong Kong SAR)
+			ZH_CN, //* Chinese (PRC)
+			ZH_SG, //* Chinese (Singapore)
+			ZU  //Zulu
+		}
+		#endregion
 	}
 }
