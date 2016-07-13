61780e86ba95002cd6fc6c7e4b35e3178f5cc407
Kostya Stankevych <kostya@zedarus.ru>
2016-07-13 13:55:51 +0300

    Update UIButtonAudio

--- UserInterface/Elements/UIButtonAudio.cs
+++ UserInterface/Elements/UIButtonAudio.cs
@@ -1,22 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 using Zedarus.ToolKit.Audio;
 
 namespace Zedarus.ToolKit.UI.Elements
 {
-	public abstract class UIButtonAudio : MonoBehaviour, IPointerClickHandler
+	public abstract class UIButtonAudio : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 	{
 		#region Parameters
 		[Header("Leave fields blank to use default IDs")]
 		[SerializeField]
 		private string _onClick = null;
+		[SerializeField]
+		private string _onPointerDown = null;
+		#endregion
+
+		#region Properties
+		private Button _button = null;
+		#endregion
+
+		#region Unity Methods
+		private void Start()
+		{
+			_button = GetComponent<Button>();
+		}
 		#endregion
 
 		#region IPointerClickHandler
 		public void OnPointerClick(PointerEventData eventData)
 		{
-			PlaySound(_onClick, DefaultClickSound);
+			if (_button != null && _button.interactable)
+			{
+				PlaySound(_onClick, DefaultClickSound);
+			}
+		}
+		#endregion
+
+		#region IPointerDownHandler
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			if (_button != null && _button.interactable)
+			{
+				PlaySound(_onPointerDown, DefaultOnPointDownSound);
+			}
 		}
 		#endregion
 
@@ -25,7 +52,12 @@ namespace Zedarus.ToolKit.UI.Elements
 		{
 			if (Audio != null)
 			{
-				if (string.IsNullOrEmpty(id.Trim()))
+				if (id != null)
+				{
+					id = id.Trim();
+				}
+
+				if (string.IsNullOrEmpty(id))
 				{
 					Audio.PlaySound(defaultID);
 				}
@@ -39,6 +71,7 @@ namespace Zedarus.ToolKit.UI.Elements
 
 		#region Getters
 		protected abstract string DefaultClickSound { get; }
+		protected abstract string DefaultOnPointDownSound { get; }
 		protected abstract AudioController Audio { get; }
 		#endregion
 	}
