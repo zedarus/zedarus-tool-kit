79c12b1f495709d0dcc22a9eb41ddc362b4206ff
zedarus <kostya@zedarus.com>
2018-07-18 17:48:15 +0300

    Add test script

---
+++ ZedarusToolKit/Assets/Plugins/ZTK/ZTKTestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZTKTestScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _prefab;
+	[SerializeField]
+	private Transform _target;
+	[SerializeField]
+	private bool _autoCreate;
+
+	private void Start () 
+	{
+		if (_autoCreate)
+		{
+			Create();
+		}
+	}
+
+	public void Create()
+	{
+		GameObject go = Instantiate(_prefab);
+		go.transform.position = _target.position;
+	}
+}
